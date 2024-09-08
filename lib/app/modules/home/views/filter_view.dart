e8d58369aa15734ef977e96e22808f98395bd165
omarmushtaha2525gmail.com <omar200234>
2024-09-09 01:20:12 +0300

    set up SfRangeSelector iniside the filter screen

--- lib/app/modules/home/views/filter_view.dart
+++ lib/app/modules/home/views/filter_view.dart
@@ -1,31 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'package:real_estate_app/app/components/common_widget/CustomWrap.dart';
-import 'package:real_estate_app/app/components/common_widget/text_widget.dart';
-import 'package:real_estate_app/app/modules/home/controllers/filter_controller.dart';
-import 'package:syncfusion_flutter_sliders/sliders.dart';
 import 'package:intl/intl.dart';
-import 'package:syncfusion_flutter_charts/charts.dart' hide LabelPlacement;
+import 'package:syncfusion_flutter_sliders/sliders.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class FilterView extends GetView<FilterController> {
-  FilterView({Key? key}) : super(key: key);
-  SfRangeValues _values = SfRangeValues(4.0, 6.0);
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FilterView(),
+    );
+  }
+}
 
+class FilterView extends StatefulWidget {
+  @override
+  State<FilterView> createState() => _FilterViewState();
+}
+
+class _FilterViewState extends State<FilterView> {
+  final double _min = 2.0;
+  final double _max = 10.0;
+  SfRangeValues _initialValues = SfRangeValues(4.5, 8.5);
+
+  final List<Data> _chartData = <Data>[
+    Data(x: 2.0, y: 2.2),
+    Data(x: 3.0, y: 3.4),
+    Data(x: 4.0, y: 2.8),
+    Data(x: 5.0, y: 1.6),
+    Data(x: 6.0, y: 2.3),
+    Data(x: 7.0, y: 2.5),
+    Data(x: 8.0, y: 2.9),
+    Data(x: 9.0, y: 3.8),
+    Data(x: 10.0, y: 3.7),
+  ];
+
+  List<Data> _filteredData = [];
+  List<Color> _columnColors = [];
+  SfRangeValues _currentValues = SfRangeValues(4.5, 8.5);
+
+  @override
+  void initState() {
+    super.initState();
+    _updateFilteredData(_initialValues);
+  }
+
+  void _updateFilteredData(SfRangeValues values) {
+    setState(() {
+      _currentValues = values;
+      _filteredData = _chartData
+          .where((data) =>
+      data.x >= values.start && data.x <= values.end)
+          .toList();
+
+      _columnColors = _chartData.map((data) {
+        if (data.x >= values.start && data.x <= values.end) {
+          return Colors.blue; // Color for selected range
+        } else {
+          return Colors.grey; // Color for non-selected range
+        }
+      }).toList();
+    });
+  }
+
+  // Function to scale the column height
+  double _scaleColumnHeight(double x) {
+    final double center = (_min + _max) / 2;
+    final double range = _max - _min;
+    final double scaledValue = 1 - ((x - center).abs() / (range / 2)).clamp(0.0, 1.0);
+
+    return scaledValue * 0.5 + 0.5; // Adjust scaling range
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(size: 20.w),
-        title: TextWidget(
-          'Filter',
-          fontSize: 20.sp,
-          fontWeight: FontWeight.w700,
-          color: const Color(0XffE86A53),
-          fontFamily: "RedHatDisplay",
-        ),
+        title: Text('Filter Example'),
         centerTitle: true,
       ),
       body: Padding(
@@ -33,22 +88,54 @@ class FilterView extends GetView<FilterController> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextWidget(
-              'Property Type',
-              fontSize: 16.sp,
-              fontWeight: FontWeight.w500,
-              color: const Color(0xFF040404),
-              fontFamily: "Satoshi",
+            Text(
+              'Select Range:',
+              style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500),
             ),
             SizedBox(height: 16.h),
-            Wrap(
-              spacing: 10.w,
-              runSpacing: 10.h,
-              children: List.generate(
-                controller.propertyType.length,
-                    (index) => FilterChipWidget(
-                  label: '${controller.propertyType[index].title}',
-                  selected: controller.propertyType[index].selected,
+            Container(
+              height: 250,
+              child: SfRangeSelector(
+                min: _min,
+                max: _max,
+                initialValues: _initialValues,
+                interval: 1,
+                showLabels: true,
+                showTicks: true,
+                numberFormat: NumberFormat("\$"),
+                onChanged: (SfRangeValues values) {
+                  _updateFilteredData(values);
+                },
+                activeColor: Colors.blue.withOpacity(0.3),
+                inactiveColor: Colors.transparent,
+                tooltipTextFormatterCallback: (dynamic value, String format) {
+                  return NumberFormat("\$").format(value);
+                },
+                child: SfCartesianChart(
+                  margin: const EdgeInsets.all(0),
+                  primaryXAxis: NumericAxis(
+                    minimum: _min,
+                    maximum: _max,
+                  ),
+                  primaryYAxis: NumericAxis(isVisible: false, maximum: 4),
+
+                  plotAreaBorderWidth: 0,
+                  plotAreaBackgroundColor: Colors.transparent,
+                  series: <ColumnSeries<Data, double>>[
+                    ColumnSeries<Data, double>(
+                      dataSource: _chartData,
+                      color: Colors.blue, // Default color (not used here)
+                      pointColorMapper: (Data data, int index) {
+                        return _columnColors[index];
+                      },
+                      xValueMapper: (Data data, int index) => data.x,
+                      yValueMapper: (Data data, int index) {
+                        double scaledHeight = _scaleColumnHeight(data.x);
+                        return data.y * scaledHeight; // Apply scaling
+                      },
+                      width: 0.4, // Column width remains constant
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -59,3 +146,8 @@ class FilterView extends GetView<FilterController> {
   }
 }
 
+class Data {
+  Data({required this.x, required this.y});
+  final double x;
+  final double y;
+}
