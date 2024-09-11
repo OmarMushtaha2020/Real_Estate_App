b6f32a51ec03eee660fa3f2c296c9683dd5c373e
omarmushtaha2525gmail.com <omar200234>
2024-09-11 15:13:46 +0300

    finsih the ui of the SfCartesianChart

--- lib/app/modules/home/controllers/filter_controller.dart
+++ lib/app/modules/home/controllers/filter_controller.dart
@@ -1,37 +1,110 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:real_estate_app/app/data_model/property.dart';
+import 'package:real_estate_app/app/modules/home/views/filter_view.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 class FilterController extends GetxController {
-  //TODO: Implement FilterController
-List <Property>propertyType=[
-  Property(selected:false,title: "All"),
-  Property(selected:false,title:"House"),
-  Property(selected:false,title:"Appartment"),
-  Property(selected:false,title:"Workspace"),
-  Property(selected:false,title:"Commercial"),
-  Property(selected:false,title:"Villa"),
-  Property(selected:false,title:"Cabin"),
-  Property(selected:false,title:"Cluster"),
+  final double min = 1000.0;
+  final double max = 10000.0;
+  Rx<SfRangeValues> initialValues = SfRangeValues(4500.0, 8500.0).obs;
+  Rx<RangeValues> rangeSliderDiscreteValues = RangeValues(40, 80).obs;
 
+  List<Data> chartData = <Data>[
+    Data(x: 1000.0, y: 2.2),
+    Data(x: 2000.0, y: 3.4),
+    Data(x: 3000.0, y: 2.8),
+    Data(x: 4000.0, y: 1.6),
+    Data(x: 5000.0, y: 2.3),
+    Data(x: 6000.0, y: 2.5),
+    Data(x: 7000.0, y: 2.9),
+    Data(x: 8000.0, y: 3.8),
+    Data(x: 9000.0, y: 3.7),
+    Data(x: 10000.0, y: 3.9),
+  ];
 
-];
+  List<Color> columnColors = [];
+  final TextEditingController minController = TextEditingController();
+  final TextEditingController maxController = TextEditingController();
 
+  final List<Property> propertyType = [
+    Property(selected: false, title: "All"),
+    Property(selected: false, title: "House"),
+    Property(selected: false, title: "Apartment"),
+    Property(selected: false, title: "Workspace"),
+    Property(selected: false, title: "Commercial"),
+    Property(selected: false, title: "Villa"),
+    Property(selected: false, title: "Cabin"),
+    Property(selected: false, title: "Cluster"),
+  ];
 
-final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    updateColumnColors(initialValues.value);
+    minController.text = initialValues.value.start.toStringAsFixed(0);
+    maxController.text = initialValues.value.end.toStringAsFixed(0);
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void updateColumnColors(SfRangeValues values) {
+    columnColors = chartData.map((data) {
+      if (data.x >= values.start && data.x <= values.end) {
+        return Colors.blue; // Color for selected range
+      } else {
+        return Colors.grey.withOpacity(0.5); // Faded color for non-selected range
+      }
+    }).toList();
+    update(); // Notify listeners
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void updateRangeFromTextFields() {
+    double minValue = double.tryParse(minController.text) ?? min;
+    double maxValue = double.tryParse(maxController.text) ?? max;
+
+    // Ensure minValue and maxValue are within valid range
+    minValue = minValue < min ? min : minValue;
+    maxValue = maxValue > max ? max : maxValue;
+
+    // Ensure minValue is less than or equal to maxValue
+    if (minValue > maxValue) {
+      maxValue = minValue; // Adjust maxValue to be at least minValue
+    }
+
+    initialValues.value = SfRangeValues(minValue, maxValue);
+    rangeSliderDiscreteValues.value = RangeValues(
+      (minValue - min) / (max - min) * 100,
+      (maxValue - min) / (max - min) * 100,
+    );
+    updateColumnColors(initialValues.value);
   }
 
-  void increment() => count.value++;
+  void onRangeSliderChanged(RangeValues values) {
+    double start = min + (values.start / 100) * (max - min);
+    double end = min + (values.end / 100) * (max - min);
+    if (start > end) {
+      end = start; // Adjust end to be at least start
+    }
+    rangeSliderDiscreteValues.value = RangeValues(
+      (start - min) / (max - min) * 100,
+      (end - min) / (max - min) * 100,
+    );
+    initialValues.value = SfRangeValues(start, end);
+    minController.text = start.toStringAsFixed(0);
+    maxController.text = end.toStringAsFixed(0);
+    updateColumnColors(initialValues.value);
+  }
+
+  double scaleColumnHeight(double x) {
+    final double center = (min + max) / 2;
+    final double range = max - min;
+    final double scaledValue = 1 - ((x - center).abs() / (range / 2)).clamp(0.0, 1.0);
+
+    return scaledValue * 0.5 + 0.5;
+  }
+}
+class Data {
+  Data({required this.x, required this.y});
+  final double x;
+  final double y;
 }
--- lib/app/modules/home/views/filter_view.dart
+++ lib/app/modules/home/views/filter_view.dart
@@ -1,223 +1,232 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:syncfusion_flutter_sliders/sliders.dart';
+import 'package:get/get.dart';
+import 'package:real_estate_app/app/components/common_widget/CustomWrap.dart';
+import 'package:real_estate_app/app/modules/home/controllers/filter_controller.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
+import 'package:real_estate_app/app/components/common_widget/text_widget.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class FilterView extends GetView<FilterController> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FilterView(),
-    );
-  }
-}
-
-class FilterView extends StatefulWidget {
-  @override
-  State<FilterView> createState() => _FilterViewState();
-}
-
-class _FilterViewState extends State<FilterView> {
-  final double _min = 1000.0;
-  final double _max = 10000.0;
-  SfRangeValues _initialValues = SfRangeValues(4500.0, 8500.0);
-  RangeValues _rangeSliderDiscreteValues = const RangeValues(40, 80);
-
-  final List<Data> _chartData = <Data>[
-    Data(x: 1000.0, y: 2.2),
-    Data(x: 2000.0, y: 3.4),
-    Data(x: 3000.0, y: 2.8),
-    Data(x: 4000.0, y: 1.6),
-    Data(x: 5000.0, y: 2.3),
-    Data(x: 6000.0, y: 2.5),
-    Data(x: 7000.0, y: 2.9),
-    Data(x: 8000.0, y: 3.8),
-    Data(x: 9000.0, y: 3.7),
-    Data(x: 10000.0, y: 3.9),
-  ];
-
-  List<Color> _columnColors = [];
-  final TextEditingController _minController = TextEditingController();
-  final TextEditingController _maxController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _updateColumnColors(_initialValues);
-    _minController.text = _initialValues.start.toStringAsFixed(0);
-    _maxController.text = _initialValues.end.toStringAsFixed(0);
-  }
-
-  void _updateColumnColors(SfRangeValues values) {
-    setState(() {
-      _columnColors = _chartData.map((data) {
-        if (data.x >= values.start && data.x <= values.end) {
-          return Colors.blue; // Color for selected range
-        } else {
-          return Colors.grey.withOpacity(0.5); // Faded color for non-selected range
-        }
-      }).toList();
-    });
-  }
-
-  void _updateRangeFromTextFields() {
-    double minValue = double.tryParse(_minController.text) ?? _min;
-    double maxValue = double.tryParse(_maxController.text) ?? _max;
-
-    // Ensure minValue and maxValue are within valid range
-    minValue = minValue < _min ? _min : minValue;
-    maxValue = maxValue > _max ? _max : maxValue;
-
-    // Ensure minValue is less than or equal to maxValue
-    if (minValue > maxValue) {
-      maxValue = minValue; // Adjust maxValue to be at least minValue
-    }
-
-    setState(() {
-      _initialValues = SfRangeValues(minValue, maxValue);
-      _rangeSliderDiscreteValues = RangeValues(
-        (minValue - _min) / (_max - _min) * 100,
-        (maxValue - _min) / (_max - _min) * 100,
-      );
-      _updateColumnColors(_initialValues);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Filter Example'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Select Range:',
-              style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500),
+    return GetBuilder<FilterController>(
+      init: FilterController(),
+      builder: (controllers){
+        return Scaffold(
+          appBar: AppBar(
+            iconTheme: IconThemeData(size: 20.w),
+            title: TextWidget(
+              'Filter',
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w700,
+              color: const Color(0XffE86A53),
+              fontFamily: "RedHatDisplay",
             ),
-            SizedBox(height: 16.h),
-            Row(
+            centerTitle: true,
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(16.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: _minController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Lowest Price',
-                      border: OutlineInputBorder(),
+                TextWidget(
+                  'Property Type',
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w500,
+                  color: const Color(0xFF040404),
+                  fontFamily: "Satoshi",
+                ),
+                SizedBox(height: 16.h),
+                Wrap(
+                  spacing: 10.w,
+                  runSpacing: 10.h,
+                  children: List.generate(
+                    controller.propertyType.length,
+                        (index) => FilterChipWidget(
+                      label: '${controller.propertyType[index].title}',
+                      selected: controller.propertyType[index].selected,
                     ),
-                    onChanged: (value) => _updateRangeFromTextFields(),
                   ),
                 ),
-                SizedBox(width: 16.w),
-                Expanded(
-                  child: TextFormField(
-                    controller: _maxController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Highest Price',
-                      border: OutlineInputBorder(),
+                Text(
+                  'Price Range:',
+                  style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 16.h),
+                Obx(() {
+                  return Container(
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        SfCartesianChart(
+                          margin: EdgeInsets.zero,
+                          primaryXAxis: NumericAxis(
+                            minimum: controllers.min,
+                            maximum: controllers.max,
+                            majorGridLines: MajorGridLines(width: 0),
+                            majorTickLines: MajorTickLines(size: 0),
+                            axisLabelFormatter: (AxisLabelRenderDetails details) {
+                              return ChartAxisLabel('', TextStyle(color: Colors.transparent));
+                            },
+                          ),
+                          primaryYAxis: NumericAxis(
+                            isVisible: false,
+                            maximum: 4,
+                          ),
+                          plotAreaBorderWidth: 0,
+                          plotAreaBackgroundColor: Colors.transparent,
+                          series: <ColumnSeries<Data, double>>[
+                            ColumnSeries<Data, double>(
+                              dataSource: controller.chartData,
+                              pointColorMapper: (Data data, int index) {
+                                return controller.columnColors[index];
+                              },
+                              xValueMapper: (Data data, int index) => data.x,
+                              yValueMapper: (Data data, int index) {
+                                double scaledHeight = controller.scaleColumnHeight(data.x);
+                                return 3 * scaledHeight; // Apply scaling
+                              },
+                              width: 0.99,
+                            ),
+                          ],
+                        ),
+                        RangeSlider(
+                          values: controller.rangeSliderDiscreteValues.value,
+                          min: 0,
+                          activeColor: Colors.blue,
+                          inactiveColor: Colors.transparent,
+                          max: 100,
+                          divisions: 5,
+                          labels: RangeLabels(
+                            controller.rangeSliderDiscreteValues.value.start.round().toString(),
+                            controller.rangeSliderDiscreteValues.value.end.round().toString(),
+                          ),
+                          onChanged: (values) {
+                            controller.onRangeSliderChanged(values);
+                          },
+                        ),
+                      ],
                     ),
-                    onChanged: (value) => _updateRangeFromTextFields(),
-                  ),
+                  );
+                }),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 37.h,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100.r),
+                          border: Border.all(color: Color(0xFFCCCCCC)),
+                        ),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            TextFormField(
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16.sp,
+                                fontFamily: "Red Hat Display",
+                              ),
+                              controller: controller.minController,
+                              keyboardType: TextInputType.number,
+                              textAlign: TextAlign.center,
+                              textAlignVertical: TextAlignVertical.center,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.only(left: 60.w, bottom: 10.h),
+                                isCollapsed: true,
+                              ),
+                              onChanged: (value) => controller.updateRangeFromTextFields(),
+                            ),
+                            Row(
+                              children: [
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(left: 20.w),
+                                    child: TextWidget(
+                                      "Min",
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16.sp,
+                                      fontFamily: "Red Hat Display",
+                                    ),
+                                  ),
+                                ),
+                                Spacer(),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(right: 20.w),
+                                    child: TextWidget(
+                                      "Max",
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16.sp,
+                                      fontFamily: "Red Hat Display",
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 8.w),
+                    Expanded(
+                      child: Container(
+                        height: 37.h,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100.r),
+                          border: Border.all(color: Color(0xFFCCCCCC)),
+                        ),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            TextFormField(
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16.sp,
+                                fontFamily: "Red Hat Display",
+                              ),
+                              controller: controller.maxController,
+                              keyboardType: TextInputType.number,
+                              textAlign: TextAlign.center,
+                              textAlignVertical: TextAlignVertical.center,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.only(left: 60.w, bottom: 10.h),
+                                isCollapsed: true,
+                              ),
+                              onChanged: (value) => controller.updateRangeFromTextFields(),
+                            ),
+                            Row(
+                              children: [
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(left: 20.w),
+                                    child: TextWidget(
+                                      "Max",
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16.sp,
+                                      fontFamily: "Red Hat Display",
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-            SizedBox(height: 16.h),
-            Container(
-              child: Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  SfCartesianChart(
-                    margin: EdgeInsets.zero,
-                    primaryXAxis: NumericAxis(
-                      minimum: _min,
-                      maximum: _max,
-                      majorGridLines: MajorGridLines(width: 0),
-                      majorTickLines: MajorTickLines(size: 0),
-                      axisLabelFormatter: (AxisLabelRenderDetails details) {
-                        return ChartAxisLabel('', TextStyle(color: Colors.transparent));
-                      },
-                    ),
-                    primaryYAxis: NumericAxis(
-                      isVisible: false,
-                      maximum: 4,
-                    ),
-                    plotAreaBorderWidth: 0,
-                    plotAreaBackgroundColor: Colors.transparent,
-                    series: <ColumnSeries<Data, double>>[
-                      ColumnSeries<Data, double>(
-                        dataSource: _chartData,
-                        pointColorMapper: (Data data, int index) {
-                          return _columnColors[index];
-                        },
-                        xValueMapper: (Data data, int index) => data.x,
-                        yValueMapper: (Data data, int index) {
-                          double scaledHeight = _scaleColumnHeight(data.x);
-                          return 3 * scaledHeight; // Apply scaling
-                        },
-                        width: 0.99,
-                      ),
-                    ],
-                  ),
-                  RangeSlider(
-                    values: _rangeSliderDiscreteValues,
-                    min: 0,
-                    activeColor: Colors.blue,
-                    inactiveColor: Colors.transparent,
-                    max: 100,
-                    divisions: 5,
-                    labels: RangeLabels(
-                      _rangeSliderDiscreteValues.start.round().toString(),
-                      _rangeSliderDiscreteValues.end.round().toString(),
-                    ),
-                    onChanged: (values) {
-                      setState(() {
-                        double start = _min + (values.start / 100) * (_max - _min);
-                        double end = _min + (values.end / 100) * (_max - _min);
-                        if (start > end) {
-                          end = start; // Adjust end to be at least start
-                        }
-                        _rangeSliderDiscreteValues = RangeValues(
-                          (start - _min) / (_max - _min) * 100,
-                          (end - _min) / (_max - _min) * 100,
-                        );
-                        _initialValues = SfRangeValues(start, end);
-                        _minController.text = start.toStringAsFixed(0);
-                        _maxController.text = end.toStringAsFixed(0);
-                        _updateColumnColors(SfRangeValues(start, end));
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
-
-  double _scaleColumnHeight(double x) {
-    final double center = (_min + _max) / 2;
-    final double range = _max - _min;
-    final double scaledValue = 1 - ((x - center).abs() / (range / 2)).clamp(0.0, 1.0);
-
-    return scaledValue * 0.5 + 0.5;
-  }
-}
-
-class Data {
-  Data({required this.x, required this.y});
-  final double x;
-  final double y;
 }
